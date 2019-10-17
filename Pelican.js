52537ca82adcf5adc6c4f9d26c058e2e1f6b25da
SantaClausNL <santaclausnl@protonmail.com>
2019-10-17 13:42:58 +0200

    Automated commit 17-10-19 13:42:58

--- Pelican.js
+++ Pelican.js
@@ -158,12 +158,15 @@ function textWidth(string, size, font) {
 //-image----------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 // function for drawing a centered image with rotation and ability to flip
 function img(x, y, image, angle, flip, width_, height_) {
+  const w, h;
+  if(defined(width_)) ctx.translate(-width_/2, -height_/2); else ctx.translate(-image.width/2, -image.height/2);
   ctx.save();
-    if(defined(ctx.rotate)) {
-
-    }
     if(flip === true) ctx.scale(-1, 1);
-    ctx.rotate(angle);
+    if(defined(angle)) {
+      if(defined(width_)) ctx.translate(-width_/2, -height_/2); else ctx.translate(-image.width/2, -image.height/2);
+      ctx.rotate(angle);
+      if(defined(width_)) ctx.translate(-width_/2, -height_/2); else ctx.translate(-image.width/2, -image.height/2);      
+    }
     ctx.translate(x, y);
     try{ if(defined(width_)) ctx.drawImage(image, 0, 0, width_, height_); else ctx.drawImage(image, 0, 0);
     } catch(err) { line([{x: -10, y: -10}, {x: 10, y: 10}], 2, 'red'); line([{x: 10, y: -10}, {x: -10, y: 10}], 2, 'red'); }
